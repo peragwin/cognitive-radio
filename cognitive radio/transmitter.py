fd1291f1f45614ee1578fa7eeb88ded777c4900a
Steven Cohen <peragwin@gmail.com>
2014-04-11 02:10:53 -0700

    not working yet

--- cognitive radio/transmitter.py
+++ cognitive radio/transmitter.py
@@ -5,10 +5,12 @@ import Queue
 import threading
 import pyaudio
 
+from paudio import *
+
 from QAM import *
 
 data = np.random.randint(0,16,size = 100)
-print data
+
 
 signal = []
 
@@ -17,10 +19,7 @@ for d in data:
 
 
 
-
-
-
-def play_audio( Q, p, fs):
+def play_audio( Q, p, fs, dev=None):
     # play_audio plays audio with sampling rate = fs
     # Q - A queue object from which to play
     # p   - pyAudio object
@@ -37,7 +36,10 @@ def play_audio( Q, p, fs):
     # p.terminate() # terminate pyAudio
     
     # open output stream
-    ostream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(fs),output=True) #,output_device_index=dev)
+    if dev:
+        ostream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(fs),output=True,output_device_index=dev)
+    else:
+        ostream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(fs),output=True) #,output_device_index=dev)
     # play audio
     while (1):
         data = Q.get()
@@ -49,9 +51,41 @@ def play_audio( Q, p, fs):
             break
 
 
+def record_audio( queue, p, fs ,dev=None,chunk=1024):
+    # record_audio records audio with sampling rate = fs
+    # queue - output data queue
+    # p     - pyAudio object
+    # fs    - sampling rate
+    # dev   - device number 
+    # chunk - chunks of samples at a time default 1024
+    #
+    # Example:
+    # fs = 44100
+    # Q = Queue.queue()
+    # p = pyaudio.PyAudio() #instantiate PyAudio
+    # record_audio( Q, p, fs, 1) # 
+    # p.terminate() # terminate pyAudio
+    
+    if dev:
+        stream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(fs),input=True,input_device_index=dev,frames_per_buffer=chunk)
+    else:
+        stream = p.open(format=pyaudio.paFloat32, channels=1, rate= int(fs),input=True,frames_per_buffer=chunk)
+
+    # record audio in chunks and append to frames
+    frames = [];
+    while (1):
+        try:  # when the pyaudio object is distroyed stops
+            data_str = istream.read(chunk) # read a chunk of data
+        except:
+            break
+        data_flt = np.fromstring( data_str, 'float32' ) # convert string to float
+        queue.put( data_flt ) # append to list
+
+
+
 # create an input output FIFO queues
 Qout = Queue.Queue()
-
+Qin = Queue.Queue()
 
 # create a pyaudio object
 p = pyaudio.PyAudio()
@@ -59,11 +93,19 @@ p = pyaudio.PyAudio()
 # initialize a playing thread. 
 t_play = threading.Thread(target = play_audio,   args = (Qout,   p, 44100  ))
 
-# start the recording and playing threads
-#t_rec.start()
+t_rec = threading.Thread(target= record_audio, args = (Qin, p, 44100) )
+# start the recording and playing threads#
+t_rec.start()
 t_play.start()
 
 # record and play about 10 seconds of audio 430*1024/44100 = 9.98 s
 Qout.put(signal)
+
+for i in range(420):
+    samples = Qin.get()
+    output = np.append(output, samples)
+
+print len(output)
+
 
 #p.terminate()
