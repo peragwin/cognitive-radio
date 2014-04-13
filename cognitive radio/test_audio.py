d63085911f2323bcf1bcbdfe8a123cf787849974
Steven Cohen <peragwin@gmail.com>
2014-04-13 11:04:25 -0700

    working audio

--- cognitive radio/test_audio.py
+++ cognitive radio/test_audio.py
@@ -2,10 +2,10 @@ from __future__ import division
 
 import numpy as np
 import scipy.signal as sp
-import Queue
-import threading
-import pyaudio
-import multiprocessing
+#import Queue
+#import threading
+#import pyaudio
+#import multiprocessing
 
 import time
 
@@ -15,14 +15,14 @@ from QAM import *
 from FSK import *
 from syncronization import *
 
-data = np.random.randint(0,16,size = 40)
+data = np.random.randint(0,32,size = 40)
 
 #signal = []
 
 #for d in data:
 #    signal = np.append(signal,modulateQAM(d,16,2000,44100,0.1))
-    
-signal = modulateFSK(data, 440.0, 4200.0, 32, 44100.0, .1)
+
+signal = modulateFSK(data, 440.0, 4200.0, 64, 44100.0, .02)
 
 signal = np.append(genSyncPulse(),signal)
 signal = np.append(signal, genSyncPulse())
@@ -31,18 +31,18 @@ signal = np.append(np.zeros(100), signal)
 signal = np.append(signal, np.zeros(100))
 
 #signal += np.random.normal(size=signal.size)
-signal = streamAndRecord(signal, 5)
+signalin = streamAndRecord(signal, 6)
 
 startTime = time.time()
 
 h = sp.firwin(24,4400, nyq = 22050)
-signal = sp.fftconvolve(signal, h)
+signal = sp.fftconvolve(signalin, h)
 signal = signal[::5]
 
 
 cropped = findSignal(signal,sync_pulse=genSyncPulse(fs=44100/5))
 
-rec_data = demodulateFSK(cropped, 440.0, 4200.0, 32, 44100.0/5, 0.1)
+rec_data = demodulateFSK(cropped, 440.0, 4200.0, 64, 44100.0/5, 0.02)
 
 
 endTime = time.time()
